b6d8c9a206554e747a745d3320062c5df5f5495c
Ivan Brunetto <ivanbrunetto@gmail.com>
2024-10-08 05:21:07 -0300

    fix booksAPI issue

--- src/BooksAPI.js
+++ src/BooksAPI.js
@@ -1,9 +1,9 @@
 const api = "https://reactnd-books-api.udacity.com";
 
-//let token = localStorage.token;
+let token = localStorage.token;
 
-//if (!token) token = localStorage.token = Math.random().toString(36).substr(-8);
-let token = Math.random().toString(36).substr(-8);
+if (!token) token = localStorage.token = Math.random().toString(36).substr(-8);
+
 const headers = {
   Accept: "application/json",
   Authorization: token,
